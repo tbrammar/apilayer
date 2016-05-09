1310f6bcf86eaa673fea4f3cce612c4c5ee077a8
Alex Fong <actfong@gmail.com>
2016-05-09 22:07:24 +0200

    renamed variable in currencylayer specs

--- spec/currency_spec.rb
+++ spec/currency_spec.rb
@@ -34,13 +34,13 @@ describe Apilayer::Currency do
     context "no currencies specified with argument" do
       it "returns a Hash with live quotes for all currencies" do
         VCR.use_cassette("currency/live_no_currencies_specified") do
-          parsed_resp = Apilayer::Currency.live
-          expect(parsed_resp).to be_a Hash
+          api_resp = Apilayer::Currency.live
+          expect(api_resp).to be_a Hash
 
-          expect(parsed_resp).to include "success"
-          expect(parsed_resp).to include "source"
-          expect(parsed_resp).to include "quotes"
-          expect(parsed_resp["quotes"]["USDAED"]).to eq 3.67295
+          expect(api_resp).to include "success"
+          expect(api_resp).to include "source"
+          expect(api_resp).to include "quotes"
+          expect(api_resp["quotes"]["USDAED"]).to eq 3.67295
         end      
       end
     end
@@ -48,13 +48,13 @@ describe Apilayer::Currency do
     context "currencies specified with argument" do
       it "returns a Hash with live quotes for specified currencies" do
         VCR.use_cassette("currency/live_with_currencies_specified") do
-          parsed_resp = Apilayer::Currency.live("EUR", "GBP", "CHF")
-          expect(parsed_resp).to be_a Hash
+          api_resp = Apilayer::Currency.live("EUR", "GBP", "CHF")
+          expect(api_resp).to be_a Hash
 
-          expect(parsed_resp["quotes"].size).to eq 3
-          expect(parsed_resp["quotes"]).to include "USDEUR"
-          expect(parsed_resp["quotes"]).to include "USDGBP"
-          expect(parsed_resp["quotes"]).to include "USDCHF"          
+          expect(api_resp["quotes"].size).to eq 3
+          expect(api_resp["quotes"]).to include "USDEUR"
+          expect(api_resp["quotes"]).to include "USDGBP"
+          expect(api_resp["quotes"]).to include "USDCHF"          
         end
       end
     end
@@ -64,10 +64,10 @@ describe Apilayer::Currency do
     context "no currencies specified as second argument" do
       it "returns exchange rates for all available currency pairs" do
         VCR.use_cassette("currency/historical_no_currency_specified") do
-          parsed_resp = Apilayer::Currency.historical("2016-05-06")
+          api_resp = Apilayer::Currency.historical("2016-05-06")
 
-          expect(parsed_resp["historical"]).to eq true
-          expect(parsed_resp["quotes"].size).to eq 168
+          expect(api_resp["historical"]).to eq true
+          expect(api_resp["quotes"].size).to eq 168
         end    
       end
     end
@@ -75,13 +75,13 @@ describe Apilayer::Currency do
     context "currencies specified as second argument" do
       it "returns only exchange rates for the specified currencies" do
         VCR.use_cassette("currency/historical_with_specified_currencies") do
-          parsed_resp = Apilayer::Currency.historical("2016-05-06", "EUR", "GBP", "CHF" )
+          api_resp = Apilayer::Currency.historical("2016-05-06", "EUR", "GBP", "CHF" )
 
-          expect(parsed_resp["historical"]).to eq true
-          expect(parsed_resp["quotes"].size).to eq 3
-          expect(parsed_resp["quotes"]).to include "USDEUR"
-          expect(parsed_resp["quotes"]).to include "USDGBP"
-          expect(parsed_resp["quotes"]).to include "USDCHF"
+          expect(api_resp["historical"]).to eq true
+          expect(api_resp["quotes"].size).to eq 3
+          expect(api_resp["quotes"]).to include "USDEUR"
+          expect(api_resp["quotes"]).to include "USDGBP"
+          expect(api_resp["quotes"]).to include "USDCHF"
         end
       end
     end
